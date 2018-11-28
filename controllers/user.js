929a796bb80b485acdf5a41dcb52cb5edca34614
LordMascachapas <32301199+LordMascachapas@users.noreply.github.com>
2018-11-28 18:24:40 +0100

    user controller added

--- controllers/user.js
+++ controllers/user.js
@@ -3,3 +3,55 @@
 const User = require('../models/user')
 const mongoose = require('mongoose')
 const service = require('../services')
+
+function createUser(req, res){
+	let user = new User()
+	
+	user.name = req.body.name
+	user.pwd = req.body.pwd
+
+	user.save((err, userStored) => {
+		if(err)
+			return res.status(500).send({message: `Error al crear usuario: ${err}`})
+		res.status(200).send({message: userStored})
+	})
+}
+
+function getUser(req, res){
+	let userId = req.params.userId
+
+	User.findById(userId, (err, user) => {
+		if(err)
+			return res.status(500).send({message: `Error al realizar peticion: ${err}`})
+		if(!user)
+			return res.status(404).send({message:`El usuario no existe`})
+		res.status(200).send({user})
+	})
+}
+
+function updateUser(req, res){
+	let updated = req.body
+
+	let userId = req.params.userId
+	User.findByIdAndUpdate(userId, updated, (err, oldUser) => {
+		if(err)
+			return res.status(500).send({message: `Error al actualizar usuario: ${err}`})
+		res.status(200).send({oldUser})
+	})
+}
+
+function deleteUser(req, res){
+	let userId = req.params.userId
+
+	User.findById(userId, (err, user) => {
+		if(err)
+			return res.status(500).send({message: `Error al borrar usuario: ${err}`})
+		if(!user)
+			return res.status(404).send({message:`El usuario no existe`})
+		user.remove(err => {
+			if(err)
+				return res.status(500).send({message: `Error al borrar usuario: ${err}`})
+			res.status(200).send({message: `El usuario ha sido borrado`})
+		})
+	})
+}
