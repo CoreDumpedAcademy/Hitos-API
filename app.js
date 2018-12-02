f3e4136e442667e0dca39ed96482eeb83faa0d0a
Angela <angela.novoa.ramos@alumnos.upm.es>
2018-12-02 23:47:00 +0100

    resolving new conflicts

--- app.js
+++ app.js
@@ -13,24 +13,15 @@ const milestoneRoutes = require('./routes/milestoneRoutes')
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
+
 app.use('/user', userRoutes);
-<<<<<<< HEAD
 app.use('express.json()');
 app.use(cors());
-app.use(sassMiddleware({
-  src: path.join(_dirname, 'public'),
-  dest: path.join(_dirname, 'public'),
-  indentedSyntax: true; //si es true es sass si es false es .alert-success
-  sourceMap: true
-}));
-
 app.use('/', routes);
 app.use('/', userRoutes);
 app.use('/', index);
-=======
 app.use('/milestone', milestoneRoutes);
 app.use('/api', api);
->>>>>>> 35283024156102ebe834f86b075513739e8bc194
 
 app.use('/milestone', milestoneRoutes);
 app.use(bodyParser.urlencoded({ extended: false}));
