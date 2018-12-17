64af388bca165897a4d8ee165c035ead998a8dff
dmerinero <davidmerineroharo@gmail.com>
2018-12-17 14:06:30 +0100

    Minor changes in enumerator get (changing object names)

--- controllers/enumerators.js
+++ controllers/enumerators.js
@@ -3,8 +3,8 @@ const enumerated = require('../middlewares/enumStructures')
 
 function sendEnum(req, res){
 	return res.status(200).send({
-		milestoneTypes: enumerated.milestoneTypes,
-	    milestoneDifficulty: enumerated.milestoneDifficulty,
+		cathegory: enumerated.milestoneTypes,
+	    difficulty: enumerated.milestoneDifficulty,
 	    teams: enumerated.teams,
 	    role: enumerated.role,
 	    status: enumerated.status
