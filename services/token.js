8cefed945b9f6a45a684d509559b900548b74977
dmerinero <davidmerineroharo@gmail.com>
2018-12-17 19:45:44 +0100

    Not returning Promise in a function corrected

--- services/token.js
+++ services/token.js
@@ -1,42 +1,44 @@
-'use strict'
+"use strict";
 
-const jwt = require('jwt-simple');
-const moment = require('moment');
-const config = require('../config');
+const jwt = require("jwt-simple");
+const moment = require("moment");
+const config = require("../config");
 
-
-function createToken (user) {
+function createToken(user) {
   const payload = {
     sub: user._id,
     iat: moment.unix(),
-    exp: moment().add(14, 'days').unix(),
-    logged: user.isLogged,
+    exp: moment()
+      .add(14, "days")
+      .unix(),
+    logged: user.isLogged
   };
   return jwt.encode(payload, config.SECRET_TOKEN);
 }
 
-function decodeToken (token){
-  const decoded = new Promise((resolve, reject)  => {
+function decodeToken(token) {
+  return new Promise((resolve, reject) => {
     try {
       const payload = jwt.decode(token, config.SECRET_TOKEN);
 
       if (payload.exp <= moment().unix()) {
-         reject({
-           status:401,
-           message: 'El token ha expirado',
-        })
+        reject({
+          status: 401,
+          message: "El token ha expirado"
+        });
       }
-      var userId = payload.sub
+      var userId = payload.sub;
       resolve(userId);
-    } catch(err) {
+    } catch (err) {
       reject({
-        status:500,
-        message: 'Token no valido',
-          })
-        }
-      })
+        status: 500,
+        message: "Token no valido"
+      });
     }
-    module.exports ={
-      createToken,
-      decodeToken
-    };
+  });
+}
+
+module.exports = {
+  createToken,
+  decodeToken
+};
