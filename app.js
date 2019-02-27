143a8fae927bdaf53d0953053c9d53f577753df6
Luis de la Cal <luiscal4a@gmail.com>
2019-02-27 16:51:00 +0100

    Added CORS to permit put requests

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', ['POST', 'PUT', 'GET'])
   res.setHeader('Access-Control-Allow-Headers', ['Content-Type', '*'])
   next();
 });
